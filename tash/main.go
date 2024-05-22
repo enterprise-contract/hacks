8f0705eea6a7cad0ccd0f34551d7fe2a991b9c03
Zoran Regvart <zoran@regvart.com>
2024-05-22 15:14:41 +0200

    Nicer handling of arguments

--- tash/main.go
+++ tash/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s path/to/recipe.yaml\n", os.Args[0])
+		os.Exit(1)
+	}
+
 	recipe := expectValue(readRecipe(os.Args[1]))
 
 	task := expectValue(readTask(recipe.Base))
