70ce0cf9e097c6492b1ba24ee4da2a7c998f5127
Simon Baird <sbaird@redhat.com>
2024-06-10 10:25:53 -0400

    Update more versions in show lastest build script
    We have a latest tag now so let's include that also, even though
    it's likely the same as 0.4.
    
    There's some reformatting also because of sbaird's neovim setup.
    (`git show -b` to see non-whitespace change.)

--- show-latest-build-versions.sh
+++ show-latest-build-versions.sh
@@ -6,45 +6,46 @@ set -o pipefail
 
 RH_TITLE="Red Hat Build"
 RH_REPO="registry.redhat.io/rhtas/ec-rhel9"
-# There's no latest tag but there is a floating tag, currently 0.2
-RH_TAG="${1:-"0.4"}"
+RH_TAGS="${1:-"0.2 0.4 latest"}"
 
 UPSTREAM_TITLE="Upstream Build"
 UPSTREAM_REPO="quay.io/enterprise-contract/ec-cli"
 UPSTREAM_TAG="snapshot"
 
 _show_details() {
-  local title="$1"
-  local ref="$2"
+	local title="$1"
+	local ref="$2"
 
-  # Make sure we have the latest
-  podman pull --quiet $ref
+	# Make sure we have the latest
+	podman pull --quiet $ref
 
-  # Get the digest
-  local digest="$(skopeo inspect "docker://$ref" | jq -r .Digest)"
+	# Get the digest
+	local digest="$(skopeo inspect "docker://$ref" | jq -r .Digest)"
 
-  echo ""
-  echo $title
-  echo $title | tr '[:print:]' '-'
+	echo ""
+	echo $title
+	echo $title | tr '[:print:]' '-'
 
-  echo Ref:
-  echo "   $ref"
+	echo Ref:
+	echo "   $ref"
 
-  echo Pinned ref:
-  echo "   $ref@$digest"
+	echo Pinned ref:
+	echo "   $ref@$digest"
 
-  echo Version:
-  podman run --rm "$ref@$digest" version | sed 's/^/   /'
+	echo Version:
+	podman run --rm "$ref@$digest" version | sed 's/^/   /'
 
-  echo Binaries in /usr/local/bin:
-  podman run --rm --entrypoint /bin/bash "$ref@$digest" -c 'ls -l /usr/local/bin' | sed 's/^/   /'
+	echo Binaries in /usr/local/bin:
+	podman run --rm --entrypoint /bin/bash "$ref@$digest" -c 'ls -l /usr/local/bin' | sed 's/^/   /'
 
-  echo Command for poking around:
-  echo "    podman run --rm -it --entrypoint /bin/bash $ref"
+	echo Command for poking around:
+	echo "    podman run --rm -it --entrypoint /bin/bash $ref"
 
-  echo ""
+	echo ""
 }
 
-_show_details "$RH_TITLE" "$RH_REPO:$RH_TAG"
+for t in ${RH_TAGS}; do
+	_show_details "$RH_TITLE" "$RH_REPO:$t"
+done
 
 _show_details "$UPSTREAM_TITLE" "$UPSTREAM_REPO:$UPSTREAM_TAG"
