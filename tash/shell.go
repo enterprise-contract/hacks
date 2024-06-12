f0f7d0bba6b18c07ef768d4719016a057915f37a
Simon Baird <sbaird@redhat.com>
2024-06-12 15:22:51 -0400

    tash: Configure formatter to put spaces after >
    Not sure why anyone would want to remove that space.

--- tash/shell.go
+++ tash/shell.go
@@ -14,7 +14,7 @@ import (
 
 var (
 	parser  = syntax.NewParser(syntax.KeepComments(true))
-	printer = syntax.NewPrinter(syntax.KeepPadding(true), syntax.Indent(2))
+	printer = syntax.NewPrinter(syntax.KeepPadding(true), syntax.Indent(2), syntax.SpaceRedirects(true))
 )
 
 func removeEnvUse(f *syntax.File, name string) []*syntax.Stmt {
